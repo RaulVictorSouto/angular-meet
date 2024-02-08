3f907befea53d9458113d5024668bddc60d31936
Raul Souto <raulvprsouto@gmail.com>
2024-02-08 18:26:43 -0300

    first commit

--- src/app/pages/reuniao/reuniao.component.ts
+++ src/app/pages/reuniao/reuniao.component.ts
@@ -1,5 +1,6 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from '@angular/core';
 import { UserService } from '../../shared.service';
+import { Session, Call } from '@apizee/apirtc';
 
 @Component({
   selector: 'app-reuniao',
@@ -11,11 +12,18 @@ import { UserService } from '../../shared.service';
 })
 export class ReuniaoComponent implements OnInit {
   userName: string = '';
+  session: Session | undefined; // Objeto de sessão apiRTC
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.userName = this.userService.userName;
     console.log("Nome de usuário recuperado:", this.userName);
-  }
+
+    //Incializa a função
+    this.session = new Session();
+
+    const call = this.session.createCall();
+    call.addStream({ audio:true, video: true});
+    call.call('outbound-media-element-id')
 }
